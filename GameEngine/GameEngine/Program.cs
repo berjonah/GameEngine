14250a7c9c49fde4774f4f3ab37a17bcdbcd04f1
Andrew Fillmore <fillmoreandrew110@gmail.com>
2015-05-29 13:41:25 -0600

    Initial Commit
    Initial setup and hello world

---
+++ GameEngine/GameEngine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+
+namespace GameEngine
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            GameWindow window = new GameWindow(640, 480);
+            window.Run();
+        }
+    }
+}
